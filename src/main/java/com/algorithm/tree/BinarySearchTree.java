4c14df6915b840a636730991eff4d62166792aff
亚克(蒋飞) <jiangfei@olayc.cn>
2020-09-10 15:10:13 +0800

    树：avl 和 rb 树

---
+++ src/main/java/com/algorithm/tree/BinarySearchTree.java
@@ -0,0 +1,125 @@
+package com.algorithm.tree;
+
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+
+    private BinaryNode<AnyType> root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void makeEmpty() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean contains(AnyType x) {
+        return contains(x, root);
+    }
+
+    public AnyType findMin() {
+        return findMin(root).element;
+    }
+
+    public AnyType findMax() {
+        return findMax(root).element;
+    }
+
+    public void insert(AnyType x) {
+        root = insert(x, root);
+    }
+
+    public void remove(AnyType x) {
+        root = remove(x, root);
+    }
+
+    private boolean contains(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null) {
+            return false;
+        }
+
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            return contains(x, t.left);
+        } else if (compareResult > 0) {
+            return contains(x, t.right);
+        } else {
+            return true;
+        }
+    }
+
+
+    private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t) {
+        if (t == null) {
+            return null;
+        } else if (t.left == null) {
+            return t;
+        } else {
+            return findMin(t.left);
+        }
+    }
+
+    private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t) {
+        if (t != null) {
+            while (t.right != null)
+                t = t.right;
+        }
+        return t;
+    }
+
+    private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null) {
+            return new BinaryNode<>(x, null, null);
+        }
+
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = insert(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = insert(x, t.right);
+        } else {
+
+        }
+        return t;
+    }
+
+    private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null) {
+            return t;
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = remove(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = remove(x, t.right);
+        } else if (t.left != null && t.right != null) {
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        } else {
+            t = (t.left != null) ? t.left : t.right;
+        }
+
+        return t;
+    }
+
+    private static class BinaryNode<AnyType> {
+
+        public BinaryNode(AnyType e) {
+            this(e, null, null);
+        }
+
+        public BinaryNode(AnyType e, BinaryNode<AnyType> l, BinaryNode<AnyType> r) {
+            element = e;
+            left = l;
+            right = r;
+        }
+
+        public AnyType element;
+        public BinaryNode<AnyType> left;
+        public BinaryNode<AnyType> right;
+    }
+
+}
