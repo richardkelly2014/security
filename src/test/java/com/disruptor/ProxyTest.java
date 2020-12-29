ca9d48f7533f562e88211d5282ad704e68d94144
亚克(蒋飞) <jiangfei@olayc.cn>
2020-12-29 11:41:13 +0800

    知识点总结

--- src/test/java/com/disruptor/ProxyTest.java
+++ src/test/java/com/disruptor/ProxyTest.java
@@ -23,14 +23,14 @@ public class ProxyTest {
     @Test
     public void test2() {
 
-        Programmer progammer = new Programmer();
+        //Programmer progammer = new Programmer();
 
         Hacker hacker = new Hacker();
         //cglib 中加强器，用来创建动态代理
         Enhancer enhancer = new Enhancer();
 
         //设置要创建动态代理的类
-        enhancer.setSuperclass(progammer.getClass());
+        enhancer.setSuperclass(Programmer.class);
 
         // 设置回调，这里相当于是对于代理类上所有方法的调用，都会调用CallBack，而Callback则需要实行intercept()方法进行拦截
         enhancer.setCallback(hacker);
